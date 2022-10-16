003f0b304efb38d713b4aac220c7560e4d20b61a
IJustWantSomeFreedom <IJustWantSomeFreedom@proton.me>
2022-10-16 22:32:39 +0400

    improve metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,15 @@ import Head from "next/head";
 import { useEffect } from "react"
 import { NotificationsProvider } from "@mantine/notifications";
 import { showNotification, NotificationProps } from "@mantine/notifications";
+import { APP_URL } from "../lib/env";
+
+const META = {
+  appName: "Sync Song Mahsa Amini",
+  description: "An offline sync pwa website playing songs about freedom",
+  appUrl: APP_URL,
+  faviconUrl: new URL("favicon.png", APP_URL).href,
+  twitterUsername: "WeAreMahsaAmini"
+}
 
 const showDefaultNotification = (props: NotificationProps) => showNotification({
   autoClose: false,
@@ -57,49 +66,39 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="application-name" content="Sync Mahsa Amini" />
+        <title>{META.appName} | Sing song synchronously</title>
+        <meta name="application-name" content={META.appName} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Sync Mahsa Amini" />
-        <meta name="description" content="An offline sync music player for freedom" />
+        <meta name="apple-mobile-web-app-title" content={META.appName} />
+        <meta name="description" content={META.description} />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
-        {/* <meta name="msapplication-TileColor" content="#2B5797" /> */}
-        {/* <meta name="msapplication-tap-highlight" content="no" /> */}
-        <meta name="theme-color" content="#000000" />
+
+        <meta name="msapplication-config" content="none" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+
+        <meta name="theme-color" content="#ffffff" />
 
         <link rel="apple-touch-icon" href="/favicon.png" />
-        {/* <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-          <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" /> */}
-
-        {/* <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" /> */}
         <link rel="manifest" href="/manifest.json" />
-        {/* <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" /> */}
         <link rel="shortcut icon" href="/favicon.png" />
 
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="Sync Mahsa Amini" />
-        <meta name="twitter:description" content="An offline sync music player for freedom" />
-        <meta name="twitter:image" content="https://yourdomain.com/favicon.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta name="twitter:url" content={META.appUrl} />
+        <meta name="twitter:title" content={META.appName} />
+        <meta name="twitter:description" content={META.description} />
+        <meta name="twitter:image" content={META.faviconUrl} />
+        <meta name="twitter:creator" content={`@${META.twitterUsername}`} />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Sync Mahsa Amini" />
-        <meta property="og:description" content="An offline sync music player for freedom" />
-        <meta property="og:site_name" content="Sync Mahsa Amini" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+        <meta property="og:title" content={META.appName} />
+        <meta property="og:description" content={META.description} />
+        <meta property="og:site_name" content={META.appName} />
+        <meta property="og:url" content={META.appUrl} />
+        <meta property="og:image" content={META.faviconUrl} />
 
-        <link rel='apple-touch-startup-image' href='/favicon.png' sizes='2048x2732' />
-        {/* <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> */}
+        <link rel='apple-touch-startup-image' href='/favicon.png' />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
